0b42b915dbb8a5502c6936cde67cdc0300842839
Greedo1977 <mmepani@gmail.com>
2020-03-15 10:44:15 +0000

    Create Recursion.scala
    Scala example of recursion

---
+++ Q3/Recursion.scala
@@ -0,0 +1,38 @@
+
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+
+
+object JsonTestApp extends App {
+  val jSonString = "{\"a\": {\"b\":{\"c\":\"d\"}}}".stripMargin
+
+  val objectMapper = new ObjectMapper() with ScalaObjectMapper
+  objectMapper.registerModule(DefaultScalaModule)
+  objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+
+  type myelement = scala.collection.Map[String, AnyRef]
+  var p : AnyRef = null
+
+  val rootNode:myelement = objectMapper.readValue[myelement](jSonString)
+  println(rootNode)
+
+  getValue(rootNode, "a")
+  println(p)
+
+  def getValue(rootNode: myelement, key: String): AnyRef = {
+    assert(!key.isEmpty)
+
+    for ((k, v) <- rootNode) {
+      if ( key == k ) // if key matches we have found it
+        p = v.toString
+      else {
+        if (!v.isInstanceOf[myelement]) // must be last element (no more maps to traverse)
+          p = "not found"
+        else
+          getValue(v.asInstanceOf[myelement], key) // call again with to search one more level
+      }
+    }
+    p
+  }
+}
